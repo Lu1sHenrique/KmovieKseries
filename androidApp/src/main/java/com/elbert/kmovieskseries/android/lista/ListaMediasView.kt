e2268105126f718fd8ab501a81a91b347760578c
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-08-02 22:34:57 -0300

    estilizando barra de pesquisa

--- androidApp/src/main/java/com/elbert/kmovieskseries/android/lista/ListaMediasView.kt
+++ androidApp/src/main/java/com/elbert/kmovieskseries/android/lista/ListaMediasView.kt
@@ -1,5 +1,6 @@
 package com.elbert.kmovieskseries.android.lista
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import com.elbert.kmovieskseries.android.search.SearchScreen
+import com.elbert.kmovieskseries.presentation.LightPinkHex
 
 @Composable
 fun ListaMediaView(navController: NavHostController) {
@@ -36,6 +38,9 @@ fun ListaMediaView(navController: NavHostController) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
+                .background(
+                    Color(LightPinkHex)
+                )
         ) {
             SearchScreen()
         }
--- androidApp/src/main/java/com/elbert/kmovieskseries/android/search/SearchScreen.kt
+++ androidApp/src/main/java/com/elbert/kmovieskseries/android/search/SearchScreen.kt
@@ -3,12 +3,14 @@ package com.elbert.kmovieskseries.android.search
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -18,7 +20,8 @@ fun SearchScreen() {
         modifier = Modifier
             .width(343.dp)
             .height(50.dp)
-            .padding(8.dp),
+            .padding(8.dp)
+            .clip(RoundedCornerShape(16.dp)),
         onSearch = { query -> searchQuery = query }
     )
 }
