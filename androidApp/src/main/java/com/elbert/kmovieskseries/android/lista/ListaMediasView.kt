83b7adbe5f2160b285090a0d19b73ce75e8062fd
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-08-03 20:55:05 -0300

    refatorando view

--- androidApp/src/main/java/com/elbert/kmovieskseries/android/lista/ListaMediasView.kt
+++ androidApp/src/main/java/com/elbert/kmovieskseries/android/lista/ListaMediasView.kt
@@ -4,20 +4,28 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.elbert.kmovieskseries.android.search.SearchScreen
+import com.elbert.kmovieskseries.android.search.SearchView
 import com.elbert.kmovieskseries.presentation.LightPinkHex
 
 @Composable
 fun ListaMediaView(navController: NavHostController) {
+    var searchQuery by remember { mutableStateOf("") }
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
@@ -42,7 +50,12 @@ fun ListaMediaView(navController: NavHostController) {
                     Color(LightPinkHex)
                 )
         ) {
-            SearchScreen()
+            SearchView(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clip(RoundedCornerShape(50.dp)),
+                onSearch = { query -> searchQuery = query }
+            )
         }
     }
 
