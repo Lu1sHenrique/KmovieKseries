4cd91af5b9dcde0a8401b9b1e787c61959c80b65
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-08-03 21:01:16 -0300

    refatorando pacote

--- shared/src/androidMain/kotlin/com/elbert/kmovieskseries/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.elbert.kmovieskseries
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
