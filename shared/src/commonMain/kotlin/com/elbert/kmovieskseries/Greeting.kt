f66006e3c136d6683b4cd96940d2e55a4bef8d77
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-08-03 21:00:33 -0300

    refatorando pacote

--- shared/src/commonMain/kotlin/com/elbert/kmovieskseries/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.elbert.kmovieskseries
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/elbert/kmovieskseries/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.elbert.kmovieskseries
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
